a8a35a40405ee9f2c82a60a02f4fcd337af480d5
rongguo97 <rongguo97@gmail.com>
2026-05-07 11:30:34 +0900

    애플리케이션 및 웹콘피그 수정.

--- foodie/backend/src/main/java/com/eatproject/backend/BackendApplication.java
+++ foodie/backend/src/main/java/com/eatproject/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.eatproject.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing; // 1. import 추가
 
+@EnableJpaAuditing // 2. JPA 감시(Auditing) 기능 활성화
 @SpringBootApplication
 public class BackendApplication {
 

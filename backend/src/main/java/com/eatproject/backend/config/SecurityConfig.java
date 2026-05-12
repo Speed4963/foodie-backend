85efcaa8f6a434afb260f4afdcb892b245728d34
Speed4963 <kim77074963@gmail.com>
2026-05-12 15:27:56 +0900

    예외 처리 추가
    SecurityConfig 예외처리 Bean 추가

--- backend/src/main/java/com/eatproject/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/eatproject/backend/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.eatproject.backend.common.jwt.AuthTokenFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -30,6 +32,11 @@ public class SecurityConfig {
     @Bean
     public AuthTokenFilter JwtTokenFilter() {
         return new AuthTokenFilter();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
     }
 
     @Bean

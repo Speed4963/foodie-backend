185cda977510a91ba6127d572fb3142dfb7d9427
ilkdy1234567-ui <ilkdy1234567@gmail.com>
2026-05-22 10:28:11 +0900

    aws연동

--- backend/src/main/java/com/eatproject/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/eatproject/backend/config/SecurityConfig.java
@@ -68,7 +68,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         // 프론트엔드 포트 허용 (3000, 5173 모두 추가)
-        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://3.38.43.101:5173", "http://localhost:3000"));
+        config.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://3.38.43.101:5173", "http://localhost:3000", "http://43.203.165.206:8080"));
         config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(Arrays.asList("*"));
         config.setAllowCredentials(true);
